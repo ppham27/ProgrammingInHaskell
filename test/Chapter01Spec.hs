37e8b767c850b76ad7294f2bdf21b1ddfc99dc78
Philip Pham <philip.pham2@gmail.com>
2018-07-08 13:49:46 -0700

    nested tests

--- test/Chapter01Spec.hs
+++ test/Chapter01Spec.hs
@@ -9,10 +9,23 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec =
+spec =  
   describe "Chapter 1" $ do
-    it "computes the product of a list of numbers" $
-      product' [3, 4, 5] `shouldBe` (60 :: Int)
+    productSpec
+    sumSpec
 
-    it "returns the first element of an *arbitrary* list" $
-      property $ \x xs -> head (x:xs) == (x :: Int)
+
+
+sumSpec :: Spec
+sumSpec = describe "sum" $
+  it "should sum a single number" $
+    property $ \x -> sum [x] == (x :: Int)  
+
+    
+productSpec :: Spec
+productSpec = describe "product" $ do
+  it "computes the product of an empty list" $
+    product' [] `shouldBe` (1 :: Int)
+  
+  it "computes the product of a list of numbers" $
+    product' [3, 4, 5] `shouldBe` (60 :: Int)
