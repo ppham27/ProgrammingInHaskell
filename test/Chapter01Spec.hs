426fe96e810bbe1c626420e2748ff9f551062066
Philip Pham <philip.pham2@gmail.com>
2018-07-08 14:20:29 -0700

    qsort tests

--- test/Chapter01Spec.hs
+++ test/Chapter01Spec.hs
@@ -1,5 +1,6 @@
 module Chapter01Spec where
 
+import Data.List
 import Test.Hspec
 import Test.QuickCheck
 
@@ -9,19 +10,16 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec =  
-  describe "Chapter 1" $ do
-    productSpec
-    sumSpec
-
-
+spec = describe "Chapter 1" $ do
+  sumSpec
+  productSpec
+  qsortSpec
 
 sumSpec :: Spec
 sumSpec = describe "sum" $
   it "should sum a single number" $
     property $ \x -> sum [x] == (x :: Int)  
 
-    
 productSpec :: Spec
 productSpec = describe "product" $ do
   it "computes the product of an empty list" $
@@ -29,3 +27,11 @@ productSpec = describe "product" $ do
   
   it "computes the product of a list of numbers" $
     product' [3, 4, 5] `shouldBe` (60 :: Int)
+
+qsortSpec :: Spec
+qsortSpec = describe "qsort" $ do
+  it "should sort a list with repeats" $
+    qsort [2,2,3,1,1] `shouldBe` ([1,1,2,2,3] :: [Int])
+      
+  it "should sort lists" $
+    property $ \xs -> qsort (xs :: [Int]) == sort xs
